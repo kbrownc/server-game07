256653d058f839d928f45164336c27201fd90da4
Kim Brown <kbrownc341@gmail.com>
2022-06-13 08:59:16 -0400

    Added push and get

--- server.js
+++ server.js
@@ -1,13 +1,39 @@
 const express = require('express');
+const Datastore = require('nedb');
 const app = express();
 
-app.get('/api/customers', (req, res) => {
-	const customers = [
-		{id: 1, firstName: 'John', lastName: 'Doe'},
-		{id: 2, firstName: 'Jane', lastName: 'Black'},
-		{id: 3, firstName: 'Cathy', lastName: 'White'}
-	];
-	res.json(customers);
+const database = new Datastore('database.db');
+const published = new Date();
+database.loadDatabase();
+// database.insert({
+// 	gameName: '1',
+// 	type: '2',
+// 	url: 'https://expo',
+// 	published: published.toDateString()
+// });
+app.use(express.static( 'public' ));
+app.use(express.json({ limit: '1mb'}));
+
+// API: Insert record into database from UI (with response back to UI)
+app.post('/api', (req, res) => {
+		const game = req.body;
+		console.log('server POST (insert)',game)
+		database.insert(game);
+		res.json({
+			status: 'success'
+		});
+});
+
+// API: query database to display on UI
+app.get('/api/games', (req, res) => {
+	database.find({}, (err, data) => {
+		console.log('server GET')
+		if (err) {
+			response.end();
+			return;
+		}
+		res.json(data);
+	});
 });
 
 const port = 5000;
