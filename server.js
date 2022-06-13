75d2d32b9490de4ec8c9e470b06a7444f67e2894
Kim Brown <kbrownc341@gmail.com>
2022-06-13 10:25:08 -0400

    Cleanyp

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ app.use(express.json({ limit: '1mb'}));
 // API: Insert record into database from UI (with response back to UI)
 app.post('/api', (req, res) => {
 		const game = req.body;
-		console.log('server POST (insert)',game)
 		database.insert(game);
 		res.json({
 			status: 'success'
@@ -27,7 +26,6 @@ app.post('/api', (req, res) => {
 // API: query database to display on UI
 app.get('/api/games', (req, res) => {
 	database.find({}, (err, data) => {
-		console.log('server GET')
 		if (err) {
 			response.end();
 			return;
