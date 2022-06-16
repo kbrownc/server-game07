bf9a3e209043b851bf0e3c860263b698bc7f8f9c
Kim Brown <kbrownc341@gmail.com>
2022-06-16 18:22:40 -0400

    Delete function working

--- server.js
+++ server.js
@@ -5,14 +5,25 @@ const app = express();
 const database = new Datastore('database.db');
 const published = new Date();
 database.loadDatabase();
-// database.insert({
-// 	gameName: '1',
-// 	type: '2',
-// 	url: 'https://expo',
-// 	published: published.toDateString()
-// });
+database.insert({
+	gameName: '1',
+	type: '2',
+	url: 'https://expo',
+	published: published.toDateString()
+});
+// console.log('delete row in db '); 
+// database.remove({_id:'0j4xm4nXG5YpM17j'});
 app.use(express.static( 'public' ));
 app.use(express.json({ limit: '1mb'}));
+
+// API: Delete record into database from UI (with response back to UI)
+app.delete('/api/games/:id', (req, res) => {
+		const id = req.params.id;
+		database.remove({_id: id});
+		res.json({
+			status: 'success'
+		});
+});
 
 // API: Insert record into database from UI (with response back to UI)
 app.post('/api', (req, res) => {
