873794733af91448bc714e9b6dcb23578d0c0d34
Kim Brown <kbrownc341@gmail.com>
2022-07-03 15:24:17 -0400

    Cleanup

--- server.js
+++ server.js
@@ -12,39 +12,40 @@ database.loadDatabase();
 // 	published: published.toDateString(),
 //  note: 'A card game for all ages'
 // });
-// console.log('delete row in db '); 
+
 // database.remove({_id:'0j4xm4nXG5YpM17j'});
+
 // database.update({_id: 'AOevL5vYceREZxr1'}, { $set: {url: 'test hard coded'}});
 
-app.use(express.static( 'public' ));
-app.use(express.json({ limit: '1mb'}));
+app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
 
 // API: Delete record into database from UI (with response back to UI)
 app.delete('/api/games/:id', (req, res) => {
-		const id = req.params.id;
-		database.remove({_id: id});
-		res.json({
-			status: 'success'
-		});
-}); 
+	const id = req.params.id;
+	database.remove({ _id: id });
+	res.json({
+		status: 'success',
+	});
+});
 
 // API: Update record into database from UI (with response back to UI)
 app.post('/api/update/:id/:url', (req, res) => {
-		const id = req.params.id;
-		const url = req.params.url;
-		database.update({_id: id}, { $set: {url: url}});
-		res.json({
-			status: 'success'
-		});
+	const id = req.params.id;
+	const url = req.params.url;
+	database.update({ _id: id }, { $set: { url: url } });
+	res.json({
+		status: 'success',
+	});
 });
 
 // API: Insert record into database from UI (with response back to UI)
 app.post('/api', (req, res) => {
-		const game = req.body;
-		database.insert(game);
-		res.json({
-			status: 'success'
-		});
+	const game = req.body;
+	database.insert(game);
+	res.json({
+		status: 'success',
+	});
 });
 
 // API: query database to display on UI
